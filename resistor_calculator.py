b5445e469368cd5bfd0867bd3de52ff5e64d1c30
Namax0r <patrykiakos@protonmail.com>
2016-11-13 17:33:01 +0100

    Modify dictionary structure

--- resistor_calculator.py
+++ resistor_calculator.py
@@ -28,36 +28,21 @@ class dotdict(dict):
 
 #create dictionary of colors and values
 d = {
-    #band
     #values of the band are stored as string to allow concantation of the numbers.
-    'black': "0",
-    'brown': "1",
-    'red': "2",
-    'orange': "3",
-    'yellow': "4",
-    'green': "5",
-    'blue': "6",
-    'violet': "7",
-    'gray': "8",
-    'white': "9",
-    #multiplier
-    'black_multiplier': 1,
-    'brown_multiplier': 10,
-    'red_multiplier': 100,
-    'orange_multiplier': 1000,
-    'yellow_multiplier': 10000,
-    'green_multiplier': 100000,
-    'blue_multiplier': 1000000,
-    'violet_multiplier': 10000000,
-    #tolerance
-    'brown_tolerance': 0.01,
-    'red_tolerance': 0.02,
-    'green_tolerance': 0.005,
-    'blue_tolerance': 0.025,
-    'violet_tolerance': 0.010,
-    'grey_tolerance': 0.005,
-    'gold_tolerance': 0.05,
-    'silver_tolerance': 0.10
+    'band':{
+        'black': "0", 'brown': "1", 'red': "2", 'orange': "3",
+        'yellow': "4", 'green': "5", 'blue': "6", 'violet': "7",
+        'gray': "8", 'white': "9"
+        },
+    'multiplier':{ 
+        'black': 1, 'brown': 10, 'red': 100, 'orange': 1000,
+        'yellow': 10000, 'green': 100000, 'blue': 1000000,
+        'violet': 10000000
+        },
+    'tolerance':{
+        'brown': 0.01, 'red': 0.02, 'green': 0.005, 'blue': 0.025,
+        'violet': 0.010, 'grey': 0.005, 'gold': 0.05, 'silver': 0.10
+        }  
     }
 
 #enable dot notation on the dictionary
@@ -184,4 +169,4 @@ class ResistorCalculator:
 if __name__ == '__main__':
     app = ResistorCalculator(root, "Resistor Calculator")
     root.mainloop()
-    
+    print(d.axa.red)
